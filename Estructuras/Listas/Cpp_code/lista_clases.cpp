1aba533b538527b639b115f1ee3b76e28d1b2d03
Romaf42 <106123923+rmoncada21@users.noreply.github.com>
2025-08-13 16:41:32 -0600

    Estructuras-Cpp-Listas: Update lista_clases.cpp. Conforms to cpplint.

--- Estructuras/Listas/Cpp_code/lista_clases.cpp
+++ Estructuras/Listas/Cpp_code/lista_clases.cpp
@@ -3,44 +3,45 @@ Lista en enlazada simple usando objetos y punteros
 */
 #include <iostream>
 
-using namespace std;
+using std::cout;
+using std::endl;
 
 // class Lista;
 
-class Nodo{
+class Nodo {
     friend class Lista;
-    private:
-        int dato;
-        Nodo* siguiente;
-    public:
-        // Constructot
-        Nodo(int d, Nodo* s) : dato(d), siguiente(s) {}
-        Nodo(int d) : dato(d), siguiente(nullptr) {}
-        int get_dato() {return dato;}
+ private:
+    int dato;
+    Nodo* siguiente;
+ public:
+    // Constructot
+    Nodo(int d, Nodo* s) : dato(d), siguiente(s) {}
+    explicit Nodo(int d) : dato(d), siguiente(nullptr) {}
+    int get_dato() {return dato;}
 };
 
-class Lista{
-    private:
-        // raíz de la lista    
-        Nodo* lista;
-    public:
-        // Constructor
-        Lista() : lista(nullptr) {}
-        
-        // Métodos
-        void crear_nodo();
-        void agregar_inicio(int dato);
-        void agregar_final(int dato);
-        void eliminar_nodo(int dato);
-        void mostrar_lista();
-        Nodo* buscar_nodo(int dato);
-        
-        // Destructor
-        // ~Lista();
+class Lista {
+ private:
+    // raíz de la lista
+    Nodo* lista;
+ public:
+    // Constructor
+    Lista() : lista(nullptr) {}
+
+    // Métodos
+    void crear_nodo();
+    void agregar_inicio(int dato);
+    void agregar_final(int dato);
+    void eliminar_nodo(int dato);
+    void mostrar_lista();
+    Nodo* buscar_nodo(int dato);
+
+    // Destructor
+    // ~Lista();
 };
 
 // Funciones de la clase Lista
-void Lista::agregar_inicio(int dato){
+void Lista::agregar_inicio(int dato) {
     Nodo* nodo = new Nodo(dato);
 
     nodo->siguiente = lista;
@@ -48,30 +49,30 @@ void Lista::agregar_inicio(int dato){
     return;
 }
 
-void Lista::mostrar_lista(){
+void Lista::mostrar_lista() {
     Nodo* temporal = nullptr;
     temporal = lista;
-    
-    while ( temporal!=nullptr ){
-        cout<<"Dato: "<<temporal->dato<<" - ";
+
+    while ( temporal != nullptr ) {
+        cout << "Dato: " << temporal->dato << " - ";
         temporal = temporal->siguiente;
     }
 
-    cout<<endl;
+    cout <<endl;
     return;
 }
 
-void Lista::agregar_final(int dato){
+void Lista::agregar_final(int dato) {
     Nodo* nodo = new Nodo(dato);
     Nodo* temporal = nullptr;
     temporal = lista;
-    
+
     // Lista vacía
-    if (temporal == nullptr){
+    if (temporal == nullptr) {
         agregar_inicio(dato);
     } else {
-        while( temporal != nullptr){
-            if(temporal->siguiente == nullptr){
+        while ( temporal != nullptr ) {
+            if (temporal->siguiente == nullptr) {
                 temporal->siguiente = nodo;
                 return;
             }
@@ -81,22 +82,21 @@ void Lista::agregar_final(int dato){
 }
 
 // TODO($user): eliminar nodo
-void Lista::eliminar_nodo(int dato){
+void Lista::eliminar_nodo(int dato) {
     Nodo* temporal = nullptr;
     temporal = lista;
     Nodo* anterior = nullptr;
 
     // Borrar primer nodo
-    if(temporal->siguiente == nullptr){
+    if (temporal->siguiente == nullptr) {
         lista = temporal->siguiente;
         delete temporal;
         return;
     }
     // Borrar nodo malcom
-    while(temporal != nullptr){
-        
-        if(temporal->dato == dato){
-            if (lista == temporal){
+    while (temporal != nullptr) {
+        if (temporal->dato == dato) {
+            if (lista == temporal) {
                 lista = temporal->siguiente;
                 delete temporal;
                 return;
@@ -113,12 +113,12 @@ void Lista::eliminar_nodo(int dato){
 }
 
 // TODO($user): Buscar nodo
-Nodo* Lista::buscar_nodo(int dato){
+Nodo* Lista::buscar_nodo(int dato) {
     Nodo* temporal = nullptr;
     temporal = lista;
 
-    while(temporal != nullptr){
-        if(temporal->dato == dato){
+    while (temporal != nullptr) {
+        if (temporal->dato == dato) {
             return temporal;
         }
         temporal = temporal->siguiente;
@@ -127,8 +127,8 @@ Nodo* Lista::buscar_nodo(int dato){
     return temporal;
 }
 
-int main(){
-    Lista lista; // objeto lista
+int main() {
+    Lista lista;  // objeto lista
     lista.agregar_inicio(5);
     lista.agregar_inicio(6);
     lista.mostrar_lista();
@@ -146,11 +146,11 @@ int main(){
     // lista_heap->mostrar_lista();
 
     Nodo* buscar = lista.buscar_nodo(6);
-    
-    if(buscar==nullptr){
-        cout<<"No existe"<<endl;
+
+    if (buscar == nullptr) {
+        cout << "No existe" << endl;
     } else {
-        cout<<"Nodo con el dato - "<<buscar->get_dato();
+        cout << "Nodo con el dato - " << buscar->get_dato();
     }
     return 0;
 }
