acdc7602fd1797082cd3cba2a16fa5bc12400164
Romaf42 <106123923+rmoncada21@users.noreply.github.com>
2025-09-01 23:47:10 -0600

    Algoritmos-Ordenamiento: Update bubble_sort.c. Conforms to cpplint --filter=-runtime/threadsafe_fn,-legal/copyright bubble_sort.c

--- Algoritmos/Ordenamiento/bubble_sort.c
+++ Algoritmos/Ordenamiento/bubble_sort.c
@@ -1,14 +1,17 @@
+/*
+Bubble sort for random array
+*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+// #include <time.h>
 
 #define SIZE 5
 void llenar_arreglo(int*, int);
 void mostrar_arreglo(int*, int);
 void bubble_sort(int*, int);
 
-int main(){
-    srand(2); // usar semilla
+int main() {
+    srand(2);  // usar semilla
     int* arreglo = (int*)malloc(sizeof(int)*SIZE);
 
     llenar_arreglo(arreglo, SIZE);
@@ -18,15 +21,15 @@ int main(){
     return 0;
 }
 
-void llenar_arreglo(int* arreglo, int size){
-    for(int i=0; i<size; i++){
+void llenar_arreglo(int* arreglo, int size) {
+    for (int i = 0; i < size; i++) {
         *(arreglo+i) = rand()%100;
     }
     return;
 }
 
-void mostrar_arreglo(int* arreglo, int size){
-    for(int i=0; i<size; i++){
+void mostrar_arreglo(int* arreglo, int size) {
+    for (int i = 0; i < size; i++) {
         int elemento = *(arreglo+i);
         printf("%d - ", elemento);
     }
@@ -35,7 +38,25 @@ void mostrar_arreglo(int* arreglo, int size){
     return;
 }
 
-// void bubble_sort(int arreglo[], int size){
+void bubble_sort(int arreglo[], int size) {
+    int swap_flag = 1;
+
+    while (swap_flag) {
+        swap_flag = 0;
+        for (int i = 0; i < (size-1); i++) {
+            if (arreglo[i] > arreglo[i+1]) {
+                int temporal = arreglo[i];
+                arreglo[i] = arreglo[i+1];
+                arreglo[i+1] = temporal;
+                swap_flag = 1;
+            }
+            mostrar_arreglo(arreglo, size);
+        }
+    }
+    return;
+}
+
+// void bubble_sort(int arreglo[], int size) {
 //     int swap = 1;
 
 //     for(int i=0; i<size; i++){
@@ -50,7 +71,7 @@ void mostrar_arreglo(int* arreglo, int size){
 //                     int temporal = dato_actual;
 //                     arreglo[j] = arreglo[j+1];
 //                     arreglo[j+1] = temporal;
-//                     // mostrar_arreglo(arreglo, size); 
+//                     //  mostrar_arreglo(arreglo, size);
 //                 }
 //                 mostrar_arreglo(arreglo, size);
 //             }
@@ -59,24 +80,3 @@ void mostrar_arreglo(int* arreglo, int size){
 //         }
 //     }
 // }
-
-
-void bubble_sort(int arreglo[], int size){
-    int swap_flag = 1;
-
-    while(swap_flag){
-        swap_flag = 0;
-
-        for(int i=0; i<(size-1); i++){
-
-            if( arreglo[i]>arreglo[i+1] ){
-                int temporal = arreglo[i];
-                arreglo[i] = arreglo[i+1];
-                arreglo[i+1] = temporal;
-                swap_flag = 1;
-            }
-            mostrar_arreglo(arreglo, size);
-        }
-    }
-    return;
-}
